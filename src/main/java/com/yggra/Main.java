c7fdfbf1068a4bafc194b6e7ac8052843089e546
karthikeyamadhavan123 <karthikeyadonnipad2005@gmail.com>
2025-07-15 12:33:18 +0530

    completed basic REPL which can take input
    # Conflicts:
    #	src/main/java/com/yggra/Main.java
    #	src/main/java/com/yggra/cli/YggraREPL.java

--- src/main/java/com/yggra/Main.java
+++ src/main/java/com/yggra/Main.java
@@ -1,26 +1,14 @@
 package com.yggra;
 
+import com.yggra.cli.YggraREPL;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         //Entry point (optional)
-        Scanner sc = new Scanner(System.in);
-        while(true){
-            try{
-                    System.out.print("Enter the command :");
-                    String command = sc.nextLine();
-                    if(command.equals("quit")){
-                        break;
-                    }
-                    else {
-                        System.out.println("command is " +command);
-                    }
-            }
-            catch (Exception e){
-                System.out.print(e);
-            }
-        }
-
+        //packages and modules set up
+        YggraREPL repl = new YggraREPL();
+        repl.start();
     }
 }
--- src/main/java/com/yggra/cli/YggraREPL.java
+++ src/main/java/com/yggra/cli/YggraREPL.java
@@ -1,5 +1,32 @@
 package com.yggra.cli;
 
+import java.util.Scanner;
+
 public class YggraREPL {
 //REPL (Read-Eval-Print Loop)
+private final Scanner sc;
+
+public YggraREPL(){
+    this.sc = new Scanner(System.in);
+}
+public void start(){
+    System.out.println("⚡ YggraDB REPL started. Type 'quit' to exit.");
+    while(true){
+        try{
+            System.out.print("yggra> ");
+            String input  = sc.nextLine().trim();
+            if(input.equalsIgnoreCase("quit")){
+                System.out.println("⚔️ Exiting YggraDB REPL. Farewell, warrior.");
+                break;
+            }
+
+            // Placeholder: Eventually pass to Lexer → Parser → SQLCommand
+            System.out.println("Received: " + input);
+        }
+        catch (Exception e){
+            System.out.println("⚠️ Error: " + e.getMessage());
+        }
+    }
+
+}
 }
